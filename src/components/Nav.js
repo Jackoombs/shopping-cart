bafc01198ee826e474fb3a271e784f700777c081
Jackoombs <jack.coombs@hotmail.co.uk>
2022-05-16 15:35:29 +0100

    Removed unused imports and added checkout nav link.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Logo from "../logo.png"
-import Cart from "../cart.png"
 import NavItem from "./NavItem";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart, faMicrochip } from '@fortawesome/free-solid-svg-icons'
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Nav({ basketCounter }) {
 
@@ -26,13 +24,13 @@ function Nav({ basketCounter }) {
           />
         </ul>
       </nav>
-      <div className="nav-checkout">
-          <p>Total amount</p>
-          <div className="cart-icon">
-            <FontAwesomeIcon icon={faShoppingCart} size="xs"/>
-            {basketCounter?<div className="basket-counter">{basketCounter}</div>:''}
-          </div>
-      </div>
+      <Link to={"/checkout"} className="nav-checkout">
+        <p>Total amount</p>
+        <div className="cart-icon">
+          <FontAwesomeIcon icon={faShoppingCart} size="xs"/>
+          {basketCounter?<div className="basket-counter">{basketCounter}</div>:''}
+        </div>
+      </Link>
     </header>
   )
 }
