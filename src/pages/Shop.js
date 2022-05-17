2885e1af44812f5c9ae0273a648568006a6af8ed
Jackoombs <jack.coombs@hotmail.co.uk>
2022-05-17 20:44:08 +0100

    Clean up code.

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -8,7 +8,15 @@ function Shop({products,basket, setBasket}) {
   return (
     <main className="product-page">
       <div className="products">
-        {products.map((product, index) => {return <Product key={index} id={index} product={product} basket={basket} setBasket={setBasket}/>})}
+        {products.map((product, index) => {
+          return  <Product 
+                    key={index} 
+                    id={index} 
+                    product={product} 
+                    basket={basket} 
+                    setBasket={setBasket}
+                  />
+          })}
       </div>
     </main>
   )
